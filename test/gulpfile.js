3cb496ecd0af78852d3a2bb360a3abeb9c4f52e2
blivesta <enmt@blivesta.com>
2015-01-25 17:55:55 +0900

    Add bump, iconfont, svg2png, image

--- test/gulpfile.js
+++ test/gulpfile.js
@@ -4,6 +4,7 @@ var del         = require('del');
 var gulp        = require('gulp');
 var pagespeed   = require('psi');
 var runSequence = require('run-sequence');
+var pkg         = require('../package.json');
 
 var autoprefixerBrowsers = ['> 1%', 'last 2 versions', 'Firefox ESR', 'Opera 12.1'];
 
@@ -32,6 +33,7 @@ module.exports = {
     src: './src/sass/test-sass.scss',
     dest: './build/scss/',
     autoprefixer: autoprefixerBrowsers,
+    pkg: pkg,
     headerBanner : true,
     banner:headerBanner,
     jekyll : false,
@@ -41,6 +43,7 @@ module.exports = {
     src: './src/less/test-less.less',
     dest: './build/less/',
     autoprefixer: autoprefixerBrowsers,
+    pkg: pkg,
     headerBanner : true,
     banner:headerBanner,
     jekyll : false,
@@ -69,6 +72,10 @@ module.exports = {
     src:  './src/images/**/*',
     dest: './build/images'
   },
+  svg2png: {
+    src:  './src/images/**/*.svg',
+    dest: './src/images'
+  },
   jekyll: {
     buildMessages : '<span style="color: grey">Running:</span> $ jekyll build'
   },
@@ -80,6 +87,19 @@ module.exports = {
   pagespeed: {
     production: 'http://example.com',
     // develop : 'http://dev.example.com'
+  },
+  bump: {
+    version: pkg.version, // base
+    src:  './bower.json', //
+    dest: '.'
+  },
+  iconfont: {
+    name:     'icon',
+    svgSrc:   './src/icon/svg/*.svg',
+    cssSrc:   './src/icon/css/template.css',
+    cssDest:  './build/icon/css',
+    fontPath: './build/icon/font/',
+    dest:     './build/icon/font'
   }
 };
 
@@ -101,13 +121,18 @@ gulp.task('bower', require('../lib/bower'));
 
 // gulp.task('imgmin', require('../lib/imgmin'));
 
+// gulp.task('svg2png', require('../lib/svg2png'));
+
 // gulp.task('jekyll-build', require('../lib/jekyll'));
 
 // gulp.task('deploy-ghpage', require('../lib/ghpage'));
 
-//
+// gulp.task('bump', require('../lib/bump'));
 
-gulp.task('uninstall', del.bind(null, module.exports.uninstall.files));
+// gulp.task('iconfont', require('../lib/iconfont'));
+
+// uninstall
+gulp.task('uni', del.bind(null, module.exports.uninstall.files));
 
 gulp.task('server', function(){ browsersync.init(null, module.exports.browsersync); });
 
